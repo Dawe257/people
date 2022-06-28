d4d0e07765e932e2180f1a42e26fc304b5ce619e
David Zhenetl <david.zhenetl@gmail.com>
2022-06-28 20:14:50 +0300

    Initial commit

---
+++ src/main/java/com/dzhenetl/Person.java
@@ -0,0 +1,85 @@
+package com.dzhenetl;
+
+public class Person {
+
+    protected final String name;
+    protected final String surname;
+    protected int age = -1;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person(String name, String surname, int age, String city) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.address = city;
+    }
+
+    public boolean hasAge() {
+        return age >= 0;
+    }
+
+    public boolean hasAddress() {
+        return address != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        age++;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        PersonBuilder builder = new PersonBuilder()
+                .setSurname(surname)
+                .setAddress(address);
+        return builder;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + age;
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/com/dzhenetl/PersonBuilder.java
@@ -0,0 +1,39 @@
+package com.dzhenetl;
+
+public class PersonBuilder {
+
+    protected String name;
+    protected String surname;
+    protected int age = -1;
+    protected String address;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Нельзя создать объект Person с отрицательным возрастом");
+        }
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        if (name == null || surname == null) {
+            throw new IllegalArgumentException("Нельзя создать объект Person без имени или фамилии");
+        }
+        return new Person(name, surname, age, address);
+    }
+}
